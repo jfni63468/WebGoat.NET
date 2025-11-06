355937921579cbf2730272816aa4f092f51a7f42
Jfni <jfni63468@edu.ucl.dk>
2025-11-06 09:59:30 +0100

    Fiks portnummer i runInDocker.sh
    Scriptet mapper port 80, men WebGoat bruger poert 8080

--- scripts/runInDocker.sh
+++ scripts/runInDocker.sh
@@ -1,6 +1,6 @@
 
 docker build --pull --rm -t webgoat.net .
-docker run --rm -d -p 5000:80 --name webgoat.net webgoat.net
+docker run --rm -d -p 5000:8080 --name webgoat.net webgoat.net
 
 echo
 echo Open http://localhost:5000 to access WebGoat.Net website
